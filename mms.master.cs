a3266c3bc1217aa40b0e93475be40ac195e773fe
BUSARI AKEEM <59509887+busak210@users.noreply.github.com>
2021-11-13 02:39:34 +0100

    first health

---
+++ mms.master.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class mms : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
